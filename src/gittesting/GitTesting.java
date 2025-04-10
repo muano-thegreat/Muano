9776875790d7b4a9caf4083ce31621f6298754a0
MuanoM <MuanoM@Lab-07.kegrp.co.za>
2025-04-10 12:20:11 +0200

    A second print statement of "hello hub"

--- src/gittesting/GitTesting.java
+++ src/gittesting/GitTesting.java
@@ -16,6 +16,7 @@ public class GitTesting {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Git");
+        System.out.println("Hello hub");
         
     }
     
